9f17766270deaa85a0d2d4c7a48ebea7c0d60a47
Holden Lippert <holdenlippert@gmail.com>
2022-11-13 15:27:33 -0500

    adopt simple-term-menu to select keep/delete

--- clean_up.py
+++ clean_up.py
@@ -1,5 +1,10 @@
 import os
 import subprocess
+from simple_term_menu import TerminalMenu
+
+
+KEEP = "Keep"
+DELETE = "Delete"
 
 
 def get_ignorelist(filename='ignorelist.txt'):
@@ -10,16 +15,23 @@ def get_ignorelist(filename='ignorelist.txt'):
         return []
 
 
+def prompt(filename):
+    options = [KEEP, DELETE]
+    title = f"What would you like to do with {filename}?"
+    terminal_menu = TerminalMenu(options, title=title)
+    selection = terminal_menu.show()
+    return options[selection]
+
+
 ignorelist = get_ignorelist()
 
 for filename in os.listdir():
     if filename in ignorelist or os.path.isdir(filename):
         continue
-    print(filename)
     subprocess.run(["open", filename])
-    # os.system(f"open {filename}")
-    if input("> ").lower() == "del":
-        print("Deleting...\n")
+    action = prompt(filename)
+    if action == DELETE:
+        print(f"Deleting {filename}")
         os.remove(filename)
-    else:
-        print("Saving.\n")
+    elif action == KEEP:
+        print(f"Keeping {filename}")
