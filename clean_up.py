5a7767f68c6cf7e1182b414fab6832bd3c24dc07
Holden Lippert <holdenlippert@gmail.com>
2022-11-13 15:03:47 -0500

    move ignorelist definition to its own file

--- clean_up.py
+++ clean_up.py
@@ -2,7 +2,15 @@ import os
 import subprocess
 
 
-ignorelist = ['Thumbs.db', '.DS_Store', '.localized', 'desktop.ini']
+def get_ignorelist(filename='ignorelist.txt'):
+    try:
+        with open(filename) as f:
+            return [line.strip() for line in f]
+    except FileNotFoundError:
+        return []
+
+
+ignorelist = get_ignorelist()
 
 for filename in os.listdir():
     if filename in ignorelist or os.path.isdir(filename):
